b814b633076644987f5063a38606d114facb229c
Aleksy Miśtal <aleksy.mistal@kontomatik.com>
2019-07-16 12:52:50 +0200

    refactor

--- src/main/java/models/Account.java
+++ src/main/java/models/Account.java
@@ -1,34 +1,34 @@
 package models;
 
 public class Account {
-    public final String accountName;
-    public final String accountBalance;
-    public final String accountCurrency;
-    public final String accountNumber;
-    public final String accountOpenDate;
-    public final String accountDescription;
-    public final String accountOwner;
+  public final String accountName;
+  public final String accountBalance;
+  public final String accountCurrency;
+  public final String accountNumber;
+  public final String accountOpenDate;
+  public final String accountDescription;
+  public final String accountOwner;
 
-    public Account(String accountName, String accountBalance, String accountCurrency, String accountNumber, String accountOpenDate, String accountDescription, String accountOwner) {
-        this.accountName = accountName;
-        this.accountBalance = accountBalance;
-        this.accountCurrency = accountCurrency;
-        this.accountNumber = accountNumber;
-        this.accountOpenDate = accountOpenDate;
-        this.accountDescription = accountDescription;
-        this.accountOwner = accountOwner;
-    }
+  public Account(String accountName, String accountBalance, String accountCurrency, String accountNumber, String accountOpenDate, String accountDescription, String accountOwner) {
+    this.accountName = accountName;
+    this.accountBalance = accountBalance;
+    this.accountCurrency = accountCurrency;
+    this.accountNumber = accountNumber;
+    this.accountOpenDate = accountOpenDate;
+    this.accountDescription = accountDescription;
+    this.accountOwner = accountOwner;
+  }
 
-    @Override
-    public String toString() {
-        return "Account{" +
-                "accountName='" + accountName + '\'' +
-                ", accountBalance='" + accountBalance + '\'' +
-                ", accountCurrency='" + accountCurrency + '\'' +
-                ", accountNumber='" + accountNumber + '\'' +
-                ", accountOpenDate='" + accountOpenDate + '\'' +
-                ", accountDescription='" + accountDescription + '\'' +
-                ", accountOwner='" + accountOwner + '\'' +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Account{" +
+            "accountName='" + accountName + '\'' +
+            ", accountBalance='" + accountBalance + '\'' +
+            ", accountCurrency='" + accountCurrency + '\'' +
+            ", accountNumber='" + accountNumber + '\'' +
+            ", accountOpenDate='" + accountOpenDate + '\'' +
+            ", accountDescription='" + accountDescription + '\'' +
+            ", accountOwner='" + accountOwner + '\'' +
+            '}';
+  }
 }
